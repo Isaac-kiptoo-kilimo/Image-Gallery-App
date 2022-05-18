a4e9178a4f0eba0768be29fc7060f29ac1b6ad81
AdnanK1 <adnangitonga@gmail.com>
2022-05-18 13:22:25 +0300

    Change views

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,8 @@ from . import main
 # from ..requests import 
 # from .forms import 
 # from ..models import User
-from .. import db,photos
+#from .. import db,photos
+
 
 # from flask_login import login_required,current_user
 
@@ -13,14 +14,12 @@ def index():
 
     return render_template('main/index.html')
 
+
 @main.route('/upload-image',methods=['GET','POST'])
 #@login_required
-def upload_image(uname):
-    user = User.query.filter_by(username = uname).first()
-    if 'photo' in request.files:
-        filename = photos.save(request.files['photo'])
-        path = f'photos/{filename}'
-        user.profile_pic_path = path
-        db.session.commit()
-        return redirect(url_for('main.index',uname=uname))
+def upload_image():
+    if request.method == 'POST':
+        if request.files:
+            image = request.files['image']
+            
     return render_template('main/upload_image.html')
