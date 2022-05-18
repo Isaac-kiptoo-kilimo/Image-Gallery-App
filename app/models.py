5d7ba7306e396dec4ed3b79b0f8aa7e88a3cd334
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-18 07:46:50 +0300

    feature:added images models

--- app/models.py
+++ app/models.py
@@ -1,23 +1,30 @@
+from pyexpat import model
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 from . import db
 from datetime import datetime
 
-
-
-    
+   
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
-
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True)
-    email = db.Column(db.String(255),unique = True,index = True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    fullname = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    bio = db.Column(db.String(255))
+    bio = db.Column(db.String(1000), nullable=False)
     profile_pic_path = db.Column(db.String())
-    password_secure = db.Column(db.String(255))
-    reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
+    password_secure = db.Column(db.String(255), nullable=False)
+    active = db.Column(db.Boolean(), default=True)
+    admin = db.Column(db.Boolean(), default=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    pitches = db.relationship('Pitch', backref='user', lazy=True)
+    comments = db.relationship('Comment', backref='user', lazy=True)
+
+    def __repr__(self):
+      return self.fullname
 
     @property
     def password(self):
@@ -30,7 +37,7 @@ class User(UserMixin,db.Model):
 
     def verify_password(self,password):
         return check_password_hash(self.password_secure,password)
-
+  
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -50,13 +57,15 @@ class Role(db.Model):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
+
 class Review(db.Model):
 
     __tablename__ = 'reviews'
 
     id = db.Column(db.Integer,primary_key = True)
-    movie_id = db.Column(db.Integer)
-    movie_title = db.Column(db.String)
+    image_id = db.Column(db.Integer)
+    image_title = db.Column(db.String)
     image_path = db.Column(db.String)
     movie_review = db.Column(db.String)
     posted = db.Column(db.DateTime,default=datetime.utcnow)
@@ -70,3 +79,10 @@ class Review(db.Model):
     def get_reviews(cls,id):
         reviews = Review.query.filter_by(movie_id=id).all()
         return reviews
+
+class Image(db:Model):
+    __tablename__='images'
+    id=db.Column(db.Integer,primary_key=True)
+    pass
+def __repr__(self):
+        return f'User {self.name}'
