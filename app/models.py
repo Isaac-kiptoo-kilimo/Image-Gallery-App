50a1878dc86d861bab46405a319251ae193e5444
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-18 08:15:07 +0300

    fix:fixing the classes in the models

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from pyexpat import model
+from tkinter import image_names, image_types
+from turtle import title
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
@@ -13,15 +15,15 @@ class User(UserMixin,db.Model):
     fullname = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    bio = db.Column(db.String(1000), nullable=False)
+    bio = db.Column(db.String(1000))
     profile_pic_path = db.Column(db.String())
     password_secure = db.Column(db.String(255), nullable=False)
     active = db.Column(db.Boolean(), default=True)
     admin = db.Column(db.Boolean(), default=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
-    pitches = db.relationship('Pitch', backref='user', lazy=True)
-    comments = db.relationship('Comment', backref='user', lazy=True)
+    images = db.relationship('Image', backref='user', lazy=True)
+    reviews = db.relationship('Review', backref='user', lazy=True)
 
     def __repr__(self):
       return self.fullname
@@ -37,26 +39,23 @@ class User(UserMixin,db.Model):
 
     def verify_password(self,password):
         return check_password_hash(self.password_secure,password)
-  
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
-
-
-    def __repr__(self):
-        return f'User {self.name}'
-
-
-
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
+
+class Image(db.Model):
+    __tablename__='images'
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(255))
+    image_name=db.Column(db.Text)
+    img=db.Column(db.Text,unique=True,nullable=False)
+    image_types=db.Column(db.Text)
+    size=db.Column(db.Integer)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    posted_at=db.Column(db.DateTime,default=datetime.utcnow)
 
 
 class Review(db.Model):
@@ -67,9 +66,9 @@ class Review(db.Model):
     image_id = db.Column(db.Integer)
     image_title = db.Column(db.String)
     image_path = db.Column(db.String)
-    movie_review = db.Column(db.String)
-    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    image_review = db.Column(db.String)
     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
 
     def save_review(self):
         db.session.add(self)
@@ -77,12 +76,8 @@ class Review(db.Model):
 
     @classmethod
     def get_reviews(cls,id):
-        reviews = Review.query.filter_by(movie_id=id).all()
+        reviews = Review.query.filter_by(image_id=id).all()
         return reviews
 
-class Image(db:Model):
-    __tablename__='images'
-    id=db.Column(db.Integer,primary_key=True)
-    pass
-def __repr__(self):
-        return f'User {self.name}'
+
+
